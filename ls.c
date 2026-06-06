83d49ae785fe82cac61e0e35e83217a74934b864
Harsh Narayan Jha <harshnj@proton.me>
2026-06-06 13:15:21 +0530

    listing directory contents

--- ls.c
+++ ls.c
@@ -1,4 +1,5 @@
 #include <dirent.h>
+#include <errno.h>
 #include <error.h>
 #include <stdio.h>
 
@@ -8,6 +9,21 @@ int main(int argc, char *argv[]) {
   DIR *dir = opendir(path);
   if (!dir) {
     perror("opendir");
+    return 1;
+  }
+
+  errno = 0;
+
+  struct dirent *entry;
+  while ((entry = readdir(dir))) {
+    if (entry->d_name[0] == '.')
+      continue;
+
+    printf("%s\n", entry->d_name);
+  }
+
+  if (errno != 0) {
+    perror("readdir");
     return 1;
   }
 
